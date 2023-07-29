555cad24852caf9f4e8de2a1ce1b8a1d98d72d20
ASIF HASAN TONMOY <92232794+asif-cse-kuet@users.noreply.github.com>
2023-07-30 01:14:36 +0600

    tailwind Css added and blade directives are used on view files

---
+++ Spentact/resources/views/header.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-100 m-0">
+ <head>
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <script src="https://cdn.tailwindcss.com"></script>
+ </head>
+ <body class="h-full m-0 font-sans">
+   <div id="app" class="bg-[purple] w-[auto] h-[auto] mx-10 text-white p-[20px"]>
+        <div class="m-2 p-4"><p>Hello From Header</p> 
+        
+    
---
+++ Spentact/resources/views/main.blade.php
@@ -0,0 +1,4 @@
+
+@section ('firstsection')
+    <p>Hello I am form main called form welcome inside welcome</p>
+@endsection
